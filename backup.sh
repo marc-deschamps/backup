19226adba10e600c2a2cef34d1d399e1182018ba
Marc Desschamps <marc.deschamps@arubacloud.fr>
2014-04-03 12:08:51 +0200

    Update conf file and created first functions.

--- backup.sh
+++ backup.sh
@@ -8,33 +8,49 @@
 ## TO AVOID RIGHT ISSUES ON FILES TO BACKUP THIS SCRIPT MUST BE RUN AS ROOT.  ##
 ################################################################################
 
-source ./backups.conf
+source ./backup.conf
+fatal_error(){
+	logger -p local1.err -t backup -- $@
+	exit 1
+}
 
-function debugger() {
-	if [[ $debug == 1 ]]; then
-		logger -p local1.debug -t backups -- $@
-	}
+debugger() {
+	if [[ $debug == "1" ]]; then
+		logger -p local1.debug -t backup -- $@
+	fi
+}
 
-function create_dest_dir() {
-	# function to create unexisting directory on backup server.
-	if [[ $@ >> 1 ]] ; then
-		
-	rsync /dev/null rsync@msvc-bkp::BACKUPS/$1
-	}
+create_host_dir() {
+	# function to create the main backup directory.
+	mkdir /tmp/${host}
+	rsync -a /tmp/${hosts}/ ${rsyncuser}@${rsyncserver}::${rsyncmodule}/${host}
+	if [ $? -eq "0" ] ; then
+		rm -R /tmp/${host}
+	else
+		fatal_error "can't create main backup directory for host $host. Exiting"
+	fi
+}
 
-function pre_backup() {
-	
-	}
+pre_backup() {
+	rsync --list-only ${rsyncuser}@${rsyncserver}::${rsyncmodule}/${host}
+	host_dir_exist=$?
+	if [[ first_time_backup -eq 1 ]] ; then 
+		create_host_dir
+		sed -i 's/first_time_backup=1/first_time_backup=0'
+	elif [[ ${host_dir_exists} -ne 0 ]] ; then
+		create_host_dir
+	fi
+}
 
-function backup_rotate() {
+backup_rotate() {
 	# First we verify if there is a backup to rotate
 	
-	}
+}
 
-function backup() {
+backup() {
 	
-	}
+}
 
-function mysql_backup() {
+mysql_backup() {
 	
-	}
+}
